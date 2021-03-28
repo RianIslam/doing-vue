11e67d0db0208036f0366734d9945746e2d5453d
RianIslam <rianislamdr@gmail.com>
2021-03-28 22:39:21 +0600

    v-if @click

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const app = Vue.createApp({
     data(){
         return{
+            showBooks: false,
             title:'the vue',
             author:'rian',
             age:23
+        }
+    },
+    methods:{
+        changeTitle(title){
+        //   this.title = 'words of randiance'  
+        this.title = title
         }
     }
 })
